9c6c28d3a74291a13a91916045dcf50f6199edc5
aa <aa>
2022-11-06 03:23:49 +0800

    save

--- cms-manage/src/App.tsx
+++ cms-manage/src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Button } from 'antd';
+
 
 export default function App() {
   return (
     <div>
       <h2>App</h2>
+      <Button type="primary">Button</Button>
     </div>
   );
 }
