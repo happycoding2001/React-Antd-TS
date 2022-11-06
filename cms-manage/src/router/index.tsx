d3d1ce5fdbeda6b0974e446dbbb936b1f25f76d7
aa <aa>
2022-11-06 22:18:53 +0800

    save

--- cms-manage/src/router/index.tsx
+++ cms-manage/src/router/index.tsx
@@ -1,6 +1,11 @@
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import React, { Suspense, lazy } from "react";
 import App from "App";
+import List from "../views/List";
+import Edit from "../views/Edit";
+import Login from "../views/Login";
+import Register from "../views/Register";
+
 
 interface Iroute {
   path: string;
@@ -23,15 +28,15 @@ let routeArr: Iroute[] = [
 
 const MyRouter = () => (
   <Router>
-    <Suspense fallback={<div>loading...</div>}>
       <Routes>
-        {
-          routeArr.map((item, index) => (
-            <Route key={index} path={item.path} element={<item.component />}></Route>
-          ))
-        }
+        <Route path="/" element={<App />} >
+          <Route path="edit" element={<Edit />} ></Route>
+          <Route path="list" element={<List />} ></Route>
+        </Route>
+        <Route path="login" element={<Login />} ></Route>
+        <Route path="register" element={<Register />} ></Route>
       </Routes>
-    </Suspense>
+
   </Router>
 );
 
