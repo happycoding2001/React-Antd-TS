26f32e1673533ec72f46bfbf235cc572376fcfc1
aa <aa>
2022-11-06 22:47:40 +0800

    save

--- cms-manage/src/router/index.tsx
+++ cms-manage/src/router/index.tsx
@@ -1,11 +1,6 @@
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import React, { Suspense, lazy } from "react";
 import App from "App";
-import List from "../views/List";
-import Edit from "../views/Edit";
-import Login from "../views/Login";
-import Register from "../views/Register";
-
 
 interface Iroute {
   path: string;
@@ -18,25 +13,41 @@ let routeArr: Iroute[] = [
     path: "/",
     component: App,
     children: [
-      { path: "edit", component: lazy(() => import("../views/Edit")) },
-      { path: "list", component: lazy(() => import("../views/List")) },
+      { path: "edit", component: lazy(() => import("views/Edit")) },
+      { path: "list", component: lazy(() => import("views/List")) },
     ],
   },
-  { path: "/login", component: lazy(() => import("../views/Login")) },
-  { path: "/register", component: lazy(() => import("../views/Register")) },
+  { path: "/login", component: lazy(() => import("views/Edit")) },
+  { path: "/register", component: lazy(() => import("views/Register")) },
 ];
+
+const renderRouter = (item:{path:string,component:React.FC<{}>},index:number):React.ReactElement | null=>{
+ return <Route key={index} path={item.path} element={<item.component />}></Route>
+}
 
 const MyRouter = () => (
   <Router>
+    <Suspense fallback={<div>loading...</div>}>
       <Routes>
-        <Route path="/" element={<App />} >
-          <Route path="edit" element={<Edit />} ></Route>
-          <Route path="list" element={<List />} ></Route>
-        </Route>
-        <Route path="login" element={<Login />} ></Route>
-        <Route path="register" element={<Register />} ></Route>
+        {
+          routeArr.map( (item, index) => {
+              if(!item.children){
+                return renderRouter(item,index)
+              }else{
+                return  <Route key={index} path={item.path} element={<item.component />}>
+                    {
+                      item.children.map( (v,i)=>{
+                        return renderRouter(v,i)
+                      } )
+                    }
+                </Route>
+              }
+              
+            }          
+          )
+        }
       </Routes>
-
+    </Suspense>
   </Router>
 );
 
